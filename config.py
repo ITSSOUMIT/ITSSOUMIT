27460885cda80caee577ec4c8525b9b20a74d5e7
Soumit Das <its.soumit.das@gmail.com>
2024-09-28 00:02:25 +0530

    added site generated from zoey

---
+++ config.py
@@ -0,0 +1,17 @@
+# Color palette for the entire site
+color_code = {
+    "red": 54,
+    "green": 189,
+    "blue": 247,
+}
+
+# Footer Text (shown on botton right of the page)
+footer_text = "Built using <a href='https://github.com/ITSSOUMIT/zoey'>Zoey</a>"
+
+# Social Links for Footer (shown on botton left of the page)
+social_mail = "mailto:me@soumit.in"
+social_linkedin = "https://www.linkedin.com/in/itssoumit/"
+social_github = "https://www.github.com/itssoumit"
+social_instagram = "https://www.instagram.com/itssoumit"
+social_twitter = ""
+social_facebook = ""
---
+++ src/contact.md
@@ -0,0 +1,12 @@
+""""
+title: Contact Soumit
+""""
+
+### Hey folks <img src="https://raw.githubusercontent.com/ABSphreak/ABSphreak/master/gifs/Hi.gif" width="30px">
+
+#### Lets connect over:
+🤙 Call: [+9189278 76631](tel:+918927876631)
+<br>
+💌 Email: [me@soumit.in](mailto:me@soumit.in)
+<br>
+🌏 Location: Bengaluru, Karnataka, India
